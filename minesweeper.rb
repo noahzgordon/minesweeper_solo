60530e938911588f149c4490054acf6531ffcf48
Noah Gordon <tortuga.ng@gmail.com>
2014-07-14 23:13:12 -0400

    Fill out the rest of the method skeleton.

--- minesweeper.rb
+++ minesweeper.rb
@@ -28,33 +28,47 @@ class Minesweeper
         flag(pos)
       when "save"
         self.save
+      else
+        command_list
       end
       
-      
-      
+      @board.display
     end
     
+    puts "You win!" if won?
+    puts "You lose!" if lost?
   end
   
   def get_input
-    #returns [command, pos]
+    puts "Please type in your command (e.g. 'click 2,3')."
+    input = gets.chomp
+    
+    input.split(' ')
+    input[1].split(',').map { |num| num.to_i } if input.count > 1
   end
   
   def click(pos)
-    
+    @board.at(pos).reveal
   end
   
   def flag(pos)
-    
+    @board.at(pos).toggle_flag
   end
   
-  
   def won?
+    tiles = @board.rows.flatten
     
+    tiles.none? { |tile| !tile.bombed && !tile.revealed }
   end
   
   def lost?
+    tiles = @board.rows.flatten
     
+    tiles.any? { |tile| tile.bombed && tile.revealed }
   end
+  
+  def command_list
+    puts "The command you entered is invaid."
+    puts "Valid commands include: 'click', 'flag', and 'save'."
   
 end
