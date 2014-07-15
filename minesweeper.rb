244ffe6395b498da4beb0b29037d1d8a171c9ae1
Noah Gordon <tortuga.ng@gmail.com>
2014-07-14 22:59:20 -0400

    Create method skeleton for Minesweeper class.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,34 @@
+require './board'
+require 'yaml'
+
 class Minesweeper
+  
+  def initialize(difficulty = :easy)
+    @board = Board.new(difficulty)
+  end
+  
+  def play
+    
+  end
+  
+  def get_input
+    
+  end
+  
+  def click(pos)
+    
+  end
+  
+  def flag(pos)
+    
+  end
+  
+  def won?
+    
+  end
+  
+  def lost?
+    
+  end
   
 end
