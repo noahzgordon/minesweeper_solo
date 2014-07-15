55c869d7b8c7efc07dd0c674883b8405efdca905
Noah Gordon <tortuga.ng@gmail.com>
2014-07-14 23:02:17 -0400

    Add basic structure of play method and save/load method skeletons.

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,16 +3,41 @@ require 'yaml'
 
 class Minesweeper
   
+  def self.save
+    
+  end
+  
+  def self.load
+    
+  end
+  
   def initialize(difficulty = :easy)
     @board = Board.new(difficulty)
   end
   
   def play
     
+    until won? || lost?
+      
+      command, pos = get_input
+      
+      case command
+      when "click"
+        click(pos)
+      when "flag"
+        flag(pos)
+      when "save"
+        self.save
+      end
+      
+      
+      
+    end
+    
   end
   
   def get_input
-    
+    #returns [command, pos]
   end
   
   def click(pos)
@@ -22,6 +47,7 @@ class Minesweeper
   def flag(pos)
     
   end
+  
   
   def won?
     
